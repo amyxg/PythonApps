bd750afe1a9a096289e5c63358a63f06741e1f93
Amy Santjer <101990402+amyxg@users.noreply.github.com>
2024-12-08 21:25:57 +0000

    app works finally

--- Lists_Flask/dateConversion.py
+++ Lists_Flask/dateConversion.py
@@ -3,7 +3,7 @@ Translate a date from mm/dd/yyyy to Month dd, yyyy format using flask
 @author: amyxg
 
 Steps for terminal
-- python -m venv env (create virtual env)
+- python -m venv env (create virtual env, only IF there is no virtual env)
 - source env/bin/activate (activate virtual env, should show (env)@amyxg ) 
 - pip install Flask
 - python dateConversion.py
@@ -12,9 +12,51 @@ from flask import Flask, request, render_template
 
 app = Flask(__name__)
 
-# Global vars
-monthNames = ["January", "February", "March", "April", "May", "June", "July",
+def dateToLabel(indate):
+    """
+    Exception handeling and converting date to Month dd, yyyy format
+
+    Parameters
+    ----------
+    indate : string
+        string of user input for date.
+
+    Returns
+    -------
+    str
+        converted date from mm/dd/yyyy to month dd, yyyy format.
+    """
+    monthNames = ["January", "February", "March", "April", "May", "June", "July",
             "August", "September", "October", "November", "December"]
+    
+    # split string into list of strings, ex: "mm", "dd", "yyyy"
+    fields = indate.split("/")
+    if len(fields) != 3:
+        return "Error: Incorrect format for date. Must be in mm/dd/yyyy format"
+    
+    month, day, year = fields
+    # check if vars are numeric, display error message if not an interger
+    if not (month.isdigit() and day.isdigit() and year.isdigit()):
+        return "Error: month (mm), day (dd), and year (yyyy) must be numbers"
+    
+    # define all vars from string to int
+    month = int(month)
+    day = int(day)
+    year = int(year)
+    
+    if month < 1 or month > 12:
+        return f"Error: {month} (mm) was not a valid month. Month should between 1 and 12."
+    
+    # accounting for leap years and max number of days in each month, Ex. Feb has 28 days
+    maxNumDays = [31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31]
+    if day < 1 or day > maxNumDays[month - 1]:
+        return f"Error: {day} (dd) was not a valid day. Day should be between 1 and {maxNumDays[month - 1]} for month {month}"
+    
+    if year < 1000 or year > 9999:
+        return f"Error: {year} (yyyy) was not a valid year. Year should be between 1000 and 9999"
+    
+    # output result in month dd, yyyy format
+    return f"{monthNames[month-1]} {day:02d}, {year}"
 
 @app.route("/", methods=['GET'])
 def home():
@@ -23,43 +65,19 @@ def home():
 
 @app.route("/dateToLabel", methods=['POST'])
 def dateConverted():
-    userInput = ""
-    while userInput != "stop":
-        
-        # requesting userDate from homepage.html
-        dateString = request.form['userDate']
-
-        # split string into list of strings, ex: "mm", "dd", "yyyy"
-        month, day, year  = dateString.split("/")
-        if len(month, day, year ) != 3:
-            return render_template('homepage.html', result="Error: Incorrect format for date. Must be in mm/dd/yyyy format")
-        
-        # check if month, day, and year are numeric, display error message if not an interger
-        if not (month.isdigit() and day.isdigit() and year.isdigit()):
-            return render_template('homepage.html', result="Error: month (mm), day (dd), and year (yyyy) must be numbers")
-
-        # define all vars from string to int
-        month = int(month)
-        day = int(day)
-        year = int(year)
-
-        if month < 1 or month > 12:
-            errorMonthMsg = f"Error: {month} (mm) was not a valid month. Month should between 1 and 12."
-            return render_template('homepage.html', result=errorMonthMsg)
-        
-        # accounting for leap years and max number of days in each month, Ex. Feb has 28 days
-        maxNumDays = [31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31]
-        if day < 1 or day > maxNumDays[month - 1]:
-            errorDayMsg = f"Error: {day} (dd) was not a valid day. Day should be between 1 and {maxNumDays[month - 1]} for month {month}"
-            return render_template('homepage.html', result=errorDayMsg)
-        
-        if year < 1000 or year > 9999:
-            errorYearMsg = f"Error: {year} (yyyy) was not a valid year. Year should be between 1000 and 9999"
-            return render_template('homepage.html', result=errorYearMsg)
-        
-        # if all conditions passes, convert that date!
-        convertedDate = f"{dateString} converted to Month dd, yyyy is {monthNames[month-1]} {day:02d}, {year}"
+    if 'choice' in request.form:
+        if request.form['choice'] == 'stop':
+            return "Goodbye.."
+        # if yes, render the form again
+        return render_template('homepage.html')
+    
+    dateString = request.form['userDate']
+    convertedDate = dateToLabel(dateString)
+    
+    if convertedDate.startswith("Error"):
         return render_template('homepage.html', result=convertedDate)
+    else:
+        return render_template('homepage.html', result=f"{dateString} converted to Month dd, yyyy is {convertedDate}")
 
     
 if __name__ == "__main__":
